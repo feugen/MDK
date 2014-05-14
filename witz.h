baaa16f4c73dc8270f55dd79a21bfe543575aa3b
feugen <eugen.fischer.android@googlemail.com>
2014-05-14 23:58:59 +0200

    Create witz.h
    Kopiert und eingefügt aus Projektdaten

---
+++ witz.h
@@ -0,0 +1,18 @@
+char frage[2];
+//int zaehler = 0;
+
+void witz (void)
+{
+        printf("\n\nHat dir dieses Programm gefallen? (j/n):");
+                fgets(frage,2,stdin);
+//Kontrollfunktion		printf("frage='%s'", frage);
+                while (frage[0] == 'n')
+//			(zaehler)+= zaehler;
+                        {printf("\n\nDu bist Scheisse und  dumm - hahaha, was hast du nun davon.");
+                        }
+                if (frage[0] == 'j')
+                        {printf("\n\nDanke, nun kannst du www.super-physik.de besuchen :-)\n\n");
+                        }
+                else {printf("Du hast die Taste nicht getroffen, aber was solls...\n\n ");
+                        }
+}
