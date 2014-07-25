6d7705dbbe5bf4044d7c2dfff1b9209a59485a85
Eugen Fischer <nuc@localhost.localdomain>
2014-07-25 02:44:33 +0200

    Etwas aufgeräumt

--- witz.h
+++ witz.h
@@ -1,18 +1,17 @@
 char frage[2];
 //int zaehler = 0;
 
-void witz (void)
-{
-        printf("\n\nHat dir dieses Programm gefallen? (j/n):");
-                fgets(frage,2,stdin);
-//Kontrollfunktion		printf("frage='%s'", frage);
-                while (frage[0] == 'n')
-//			(zaehler)+= zaehler;a
-                        {printf("\n\nDu bist Scheisse und  dumm - hahaha, was hast du nun davon.");
-                        }
-                if (frage[0] == 'j')
-                        {printf("\n\nDanke, nun kannst du www.super-physik.de besuchen :-)\n\n");
-                        }
-                else {printf("Du hast die Taste nicht getroffen, aber was solls...\n\n ");
-                        }
+void witz (void){
+    printf("\n\nHat dir dieses Programm gefallen? (j/n):");
+    fgets(frage,2,stdin);
+    //Kontrollfunktion printf("frage='%s'", frage);
+    //(zaehler)+= zaehler;
+    while (frage[0] == 'n'){
+        printf("\n\nDu bist Scheisse und  dumm - hahaha, was hast du nun davon.");
+    }
+    if (frage[0] == 'j'){
+        printf("\n\nDanke, nun kannst du www.super-physik.de besuchen :-)\n\n");
+    }
+    else {printf("Du hast die Taste nicht getroffen, aber was solls...\n\n ");
+    }
 }
